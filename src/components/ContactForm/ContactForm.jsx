c5cee27203c2c898aa7060732735165bae8e1651
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-09-04 21:13:20 +0300

    make-form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,198 @@
-import { Component } from "react";
+import { useState } from "react";
 import { nanoid } from 'nanoid'
 import { ButtonAddContact, FormTable, InputInfToAdd, Label } from "components/App.styled";
 
-export class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''        
+
+export const ContactForm = () => {
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
+
+ const handleImputChange = e => {
+    const {name, value} = e.target;
+    
+    switch(name){
+      case 'name':
+        setName(value);
+        break;
+        case 'number':
+        setNumber(value);
+        break;
+
+        default:
+          return;
     }
-    handleImputChange = e => {
-        const {name, value} = e.target;
-        
-        this.setState({[name]: value})
-      }
+    // this.setState({[name]: value})
+  }
 
-      handleAddContact = e => {
-        e.preventDefault();
-        const id = nanoid();
-        this.props.onSubmit({...this.state, id});
-        this.reset();
-      }
+  const handleAddContact = e => {
+    e.preventDefault();
+    const id = nanoid();
+    onSubmit({name, number, id});
+    reset();
+  }
 
-      reset = () => {
-        this.setState({name: '', number: ''})
-      }
+  const reset = () => {
+    setName('')
+    setNumber('')
+    // this.setState({name: '', number: ''})
+  }
 
-    render (){
-        return(
-            <div>              
-                <FormTable onSubmit={this.handleAddContact}>        
-        <Label htmlFor="">
-        Name
-        <InputInfToAdd
-           type="text"
-           name="name"
-           value={this.state.name}
-           onChange={this.handleImputChange}
-           pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-           required
-          />
-          </Label>
+
+  return (
+    <div>              
+                <FormTable onSubmit={handleAddContact}>        
+                        <Label htmlFor="">
+                              Name
+                              <InputInfToAdd
+                                 type="text"
+                                name="name"
+                                value={name}
+                                 onChange={handleImputChange}
+                                 pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                                 required
+                                />
+                        </Label>
                 
          
-         <Label htmlFor="">Number
-         <InputInfToAdd
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleImputChange}
-              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-            </Label>
+                        <Label htmlFor="">Number
+                               <InputInfToAdd
+                                    type="tel"
+                                    name="number"
+                                    value={number}
+                                    onChange={handleImputChange}
+                                    pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+                                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                                    required
+                                                       />
+                            </Label>
             
-         <ButtonAddContact type="submit" >Add contact</ButtonAddContact>
-        </FormTable>
+                        <ButtonAddContact type="submit" >Add contact</ButtonAddContact>
+                </FormTable>
             </div>
-        )
-    }
+  )
 }
+
+
+
+// export class ContactForm extends Component {
+//     state = {
+//         name: '',
+//         number: ''        
+//     }
+//     handleImputChange = e => {
+//         const {name, value} = e.target;
+        
+//         this.setState({[name]: value})
+//       }
+
+//       handleAddContact = e => {
+//         e.preventDefault();
+//         const id = nanoid();
+//         this.props.onSubmit({...this.state, id});
+//         this.reset();
+//       }
+
+//       reset = () => {
+//         this.setState({name: '', number: ''})
+//       }
+
+//     render (){
+//         return(
+//             <div>              
+//                 <FormTable onSubmit={this.handleAddContact}>        
+//                         <Label htmlFor="">
+//                         Name
+//                         <InputInfToAdd
+//                            type="text"
+//                            name="name"
+//                            value={this.state.name}
+//                            onChange={this.handleImputChange}
+//                            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                            required
+//                           />
+//                           </Label>
+                
+         
+//                          <Label htmlFor="">Number
+//          <InputInfToAdd
+//               type="tel"
+//               name="number"
+//               value={this.state.number}
+//               onChange={this.handleImputChange}
+//               pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//               required
+//             />
+//                             </Label>
+            
+//                         <ButtonAddContact type="submit" >Add contact</ButtonAddContact>
+//                 </FormTable>
+//             </div>
+//         )
+//     }
+// }
+
+// import { Component } from "react";
+// import { nanoid } from 'nanoid'
+// import { ButtonAddContact, FormTable, InputInfToAdd, Label } from "components/App.styled";
+
+// export class ContactForm extends Component {
+//     state = {
+//         name: '',
+//         number: ''        
+//     }
+//     handleImputChange = e => {
+//         const {name, value} = e.target;
+        
+//         this.setState({[name]: value})
+//       }
+
+//       handleAddContact = e => {
+//         e.preventDefault();
+//         const id = nanoid();
+//         this.props.onSubmit({...this.state, id});
+//         this.reset();
+//       }
+
+//       reset = () => {
+//         this.setState({name: '', number: ''})
+//       }
+
+//     render (){
+//         return(
+//             <div>              
+//                 <FormTable onSubmit={this.handleAddContact}>        
+//         <Label htmlFor="">
+//         Name
+//         <InputInfToAdd
+//            type="text"
+//            name="name"
+//            value={this.state.name}
+//            onChange={this.handleImputChange}
+//            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//            required
+//           />
+//           </Label>
+                
+         
+//          <Label htmlFor="">Number
+//          <InputInfToAdd
+//               type="tel"
+//               name="number"
+//               value={this.state.number}
+//               onChange={this.handleImputChange}
+//               pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//               required
+//             />
+//             </Label>
+            
+//          <ButtonAddContact type="submit" >Add contact</ButtonAddContact>
+//         </FormTable>
+//             </div>
+//         )
+//     }
+// }
