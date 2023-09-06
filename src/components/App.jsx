ac1a2279e63f6d8cde7152f41812ae38f6c2cd3e
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-09-06 10:30:16 +0300

    remake-initial-value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,21 @@ import { ContactForm } from "./ContactForm/ContactForm"
 import { ContactList } from "./ContactList/ContactList"
 import { Filter } from "./Filter/Filter"
 
-export const App = () =>{
-  const [contacts, setContacts] =useState(
-    () => JSON.parse(window.localStorage.getItem('contacts')) ?? [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-])
-const [filter, setFilter] = useState('')
 
-const formSubmitHendle = data => {
-      const returnContact = contacts.find(contact => contact.name.toLowerCase().includes(data.name.toLowerCase()) );
+const getInitialFilters= () => {
+    return  JSON.parse(window.localStorage.getItem('contacts')) ?? [
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ]
+}
+export const App = () =>{
+      const [contacts, setContacts] =useState(getInitialFilters)
+      const [filter, setFilter] = useState('')
+
+      const formSubmitHendle = data => {
+            const returnContact = contacts.find(contact => contact.name.toLowerCase().includes(data.name.toLowerCase()) );
 
       if(returnContact){
          return alert(`${data.name} is already in contacts`)
